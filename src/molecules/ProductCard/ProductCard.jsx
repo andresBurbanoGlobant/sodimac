0a57116b3c8ee3b0b6938fc4721bb05a67a3cd46
Andres Burbano <andres.burbano@globant.com>
2022-06-03 10:40:18 -0500

    Add some styles.

--- src/molecules/ProductCard/ProductCard.jsx
+++ src/molecules/ProductCard/ProductCard.jsx
@@ -2,12 +2,20 @@ import styles from './ProductCard.module.css'
 
 const ProductCard = ({name, sku, price, image}) => {
   return (
-    <div className={styles.card}>
-      <p>{name}</p>
-      <p>{sku}</p>
-      <p>{price}</p>
-      <p>{image}</p>
-    </div>
+    <li className={styles.card}>
+      <div className={styles.card_content}>
+        <div className={styles.card_image}>
+          <img src={image} alt={name} />
+        </div>
+        <div className={styles.card_info}>
+          <div className={styles.product_info}>
+            <h2>{name}</h2>
+            <p>{`sku: ${sku}`}</p>
+          </div>
+          <p className={styles.product_price}>{`$ ${price}`}</p>
+        </div>
+      </div>
+    </li>
   )
 }
 
